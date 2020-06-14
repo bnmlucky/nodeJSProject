50c850805480caeb0ae7eb1a58a5c90dd7dcd82f
Natalia Titova <bnmlucky@gmail.com>
2020-06-14 10:11:11 -0400

    will need to fix the last try catch block

--- weather.js
+++ weather.js
@@ -7,23 +7,51 @@ function printWeather(weather) {
     console.log(message);
 }
 // Print out error message
-
-function get(query) {
-    const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, response => {
-        let body = "";
-        // Read the data
-        response.on('data', chunk => {
-            body += chunk;
-        });
-        response.on('end', () => {
-            const weather = JSON.parse(body);
-            // Parse data
-            printWeather(weather);
-            // Print the data
-        });
-    });
+function printError(error) {
+    console.error(error.message);
 }
 
+function get(query) {
+    //Take out underscores for readability
+    const readableQuery = query.replace('_', '');
+    try {
+        const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, response => {
+            if (response.statusCode === 200) {
+                let body = "";
+                // Read the data
+                response.on('data', chunk => {
+                    body += chunk;
+                });
+                response.on('end', () => {
+                    try {
+                        const weather = JSON.parse(body);
+                        // Check if the location was found before pringing
+                        if (weather.location) {
+                            // Pirnt the data
+                            printWeather(weather);
+                        } else {
+                            const queryError = new Error(`The location "${readableQuery} was not found.`);
+                            printError(queryError);
+                        }
+                    } catch (error) {
+                        // Parse Error
+                        printError(error);
+                    }
+                    // Parse data
+                    printWeather(weather);
+                    // Print the data
+                });
+            } else {
+                // Status Code Error
+                const statusCodeError = new Error(`There was an error getting the message for ${readableQuery}. (${http.STATUS_CODES[response.statusCode]})`);
+                printError(statusCodeError);
+            }
+         catch (error) {
+                printError(error);
+            }
+        }
+
+    
 module.exports.get = get;
 
 // TODO: Handle any errors
